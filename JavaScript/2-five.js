bbf919f0f453db1f6e1b800bf905e8ee206591f7
trotskyy <trotsko161@gmail.com>
2016-11-29 15:05:47 +0200

    Node.setParent() added
    IT-31, Trotskyy M

--- JavaScript/2-five.js
+++ JavaScript/2-five.js
@@ -1,4 +1,5 @@
 'use strict';
+//тимур гафарович
 
 function Tree(data) {
   this.count = 1;
@@ -7,9 +8,15 @@ function Tree(data) {
 }
 
 function Node(parent, data) {
-  this.data = data;
+
+  this.prev = null;//parent
+  this.next = null;//parent
+  this.first = null;//child
+  this.last = null;//child
+
+  this.data = data;//content
   this.parent = parent;
-  this.count = 0;
+  this.count = 0;//child amount
   if (parent) {
     this.tree = parent.tree;
     this.tree.count++;
@@ -23,10 +30,47 @@ function Node(parent, data) {
     parent.last = this;
     parent.count++;
   }
-  this.prev = null;
-  this.next = null;
-  this.first = null;
-  this.last = null;
+  
+
+  this.setParent = function(node /*new parent node*/){
+  	//1
+  	if(this.parent.count ==1)
+  	{
+  		this.parent.first = null;
+  		this.parent.last = null;
+  	}
+  	else 
+  	{
+  		if(this == this.parent.first)
+  			this.parent.first = this.prev;
+  		else if(this == this.parent.last)
+  			this.parent.last = this.next;
+  	}
+
+  	//2
+  	if(this.parent.count != 1)
+  	{
+	  	this.prev.next = this.next;
+	  	this.next.prev = this.prev;
+	}
+  	this.parent.count--;
+
+  	//3
+  	if(node.count == 0)
+  	{
+  		node.first = this;
+  		node.last = this;
+  	}
+  	else
+  	{
+  		node.last.next = this;
+  		node.last = this;
+  	}
+
+  	node.count++;
+  	this.parent = node;
+
+  }
 }
 
 let tree = new Tree({ name: 'root' });
